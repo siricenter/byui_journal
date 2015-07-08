c5306a3d001f3244049e5e5a19572219cebe71c7
Evan Caldwell <evancaldwell@rbdc1-OptiPlex-755.(none)>
2015-07-08 16:50:27 -0600

    altered the DAL reference and added departments and articles table definitions.

--- models/db.py
+++ models/db.py
@@ -11,7 +11,7 @@
 
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
+    db = DAL('sqlite://../../rcwc/databases/storage.sqlite',pool_size=1,check_reserved=['all'], migrate_enabled=False)
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
     db = DAL('google:datastore')
